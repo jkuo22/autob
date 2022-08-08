4e3f8f1c827353a6f1d33da4c1bd3ae04db973c8
jkuo22 <jkuo22@gmail.com>
2022-08-08 15:56:53 +0800

    recover debugging code.

--- paymentNotices.py
+++ paymentNotices.py
@@ -12,12 +12,11 @@ days = dict( ( day, date(today.year, today.month, day) )
 month = today.month if today.month in range(2, 13, 3) else 8
 quarterly8 = date(today.year, month, 8)
 
-#for _ in range(3):
-#    Customer('max188', 'Maxx', base=2, billday=quarterly8 )
-
 Customer('gorich1688', '阿勇', base=4, billday=days[21])
+#for _ in range(3):
+#    Customer('maxxx188', 'MAX', base=2, billday=quarterly8 )
+
 Customer('grace', 'Grace', adv=1, billday=days[10])
 Customer('hamburger', 'Hamburger', base=1, adv=2, billday=days[15])
-Customer('ding', 'Ding', base=2, billday=days[1])
-#Customer('gao', 'Gao', base=1, billday=days[6])
+Customer('ding', 'Ding', base=2, billday=days[23])
 #Customer('autobilling_test', '自動繳費單測試', base=1, adv=1, billday=today)
