ffcb2077b4d3e2c31ec9f40a13af8edf96c4c57d
jkuo22 <jkuo22@gmail.com>
2022-08-14 19:39:38 +0800

    'MAX' modified, 'LV' added.

--- paymentNotices.py
+++ paymentNotices.py
@@ -13,10 +13,9 @@ month = today.month if today.month in range(2, 13, 3) else 8
 quarterly8 = date(today.year, month, 8)
 
 Customer('gorich1688', '阿勇', base=4, billday=days[21])
-#for _ in range(3):
-#    Customer('maxxx188', 'MAX', base=2, billday=quarterly8 )
-
+Customer('maxxx188', 'MAX', base=6, billday=quarterly8 )
 Customer('grace', 'Grace', adv=1, billday=days[10])
 Customer('hamburger', 'Hamburger', base=1, adv=2, billday=days[15])
 Customer('ding', 'Ding', base=2, billday=days[23])
+Customer('lv', 'LV', adv=1, billday=days[3])
 #Customer('autobilling_test', '自動繳費單測試', base=1, adv=1, billday=today)
