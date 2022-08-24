0eda50d2dff165149314a7ef1b337cd750373132
jkuo22 <jkuo22@gmail.com>
2022-08-24 08:59:02 +0800

    remove unused code.

--- paymentNotices.py
+++ paymentNotices.py
@@ -10,16 +10,6 @@ days = dict(
     for day in range( 1, lastday_month+1 )
     )
 
-#month = today.month if today.month in range(2, 13, 3) else 8
-#quarterly8 = date(today.year, month, 8)
-#
-#Customer('gorich1688', '阿勇', base=4, billday=days[21])
-#Customer('maxxx188', 'MAX', base=6, billday=quarterly8 )
-#Customer('grace', 'Grace', adv=1, billday=days[10])
-#Customer('hamburger', 'Hamburger', base=1, adv=2, billday=days[15])
-#Customer('ding', 'Ding', base=1, billday=days[23])
-#Customer('lv', 'LV', adv=1, billday=days[3])
-#Customer('autobilling_test', '自動繳費單測試', base=1, adv=1, billday=today)
 
 with open('payers.json', 'r') as jsonf:
     loadPayers = json.load( jsonf )
